5419db08fe29f56d4b12c9fccd65be95cf97e8d1
Krish3017 <krishramanandi2007@gmail.com>
2025-12-13 20:24:42 +0530

    adding the files

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import List from './pages/list/List'
+import New from './pages/new/New'
+import Single from './pages/single/Single'
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+const App = () => {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='users'>
+              <Route index element={<List />} />
+              <Route path=':userid' element={<Single />} />
+              <Route path='new' element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.scss'
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <div className="hello">Hi</div>
+    </div>
+  )
+}
+
+export default Home
